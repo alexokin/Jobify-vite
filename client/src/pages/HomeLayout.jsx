e3b0044a6b771472face320d39ed10d33f08f52f
alexokin <alex.okin@gmail.com>
2023-08-31 12:31:47 +0300

    register and login pages

--- client/src/pages/HomeLayout.jsx
+++ client/src/pages/HomeLayout.jsx
@@ -3,7 +3,7 @@ import { Outlet } from "react-router-dom";
 const HomeLayout = () => {
   return (
     <div>
-      <div>HomeLayout</div>
+      
       <Outlet />
     </div>
   );
--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,10 +1,29 @@
 import { Link } from "react-router-dom";
+import Wrapper from "../assets/wrappers/RegisterAndLoginPage";
+import { FormRow, Logo } from "../components";
 const Register = () => {
   return (
-    <div>
-      <h1>Register Page</h1>
-      <Link to='/login'>Login Page</Link>
-    </div>
+    <Wrapper>
+      <form className="form">
+        <Logo />
+        <h4>Register</h4>
+        <FormRow type='text' name='name' defaultValue='alex'/>
+        <FormRow type='text' name='lastName' labelText='last name' defaultValue='okin'/>
+        <FormRow type='text' name='location' defaultValue='earth'/>
+        <FormRow type='email' name='email' defaultValue='alex.okin@gmail.com'/>
+        <FormRow type='password' name='password' defaultValue='123456'/>
+
+        <button type="submit" className="btn btn-block">
+          submit
+        </button>
+        <p>
+          Already a member?
+          <Link to="/login" className="member-btn">
+            Login
+          </Link>
+        </p>
+      </form>
+    </Wrapper>
   );
 };
 
