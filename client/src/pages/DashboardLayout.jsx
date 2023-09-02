1b701c6aa4a06ef70d9e13f4565ca505ca541dd1
alexokin <alex.okin@gmail.com>
2023-09-02 11:39:32 +0300

    delete readme

--- client/src/pages/DashboardLayout.jsx
+++ client/src/pages/DashboardLayout.jsx
@@ -1,7 +1,24 @@
 import { Outlet } from "react-router-dom";
 import Wrapper from "../assets/wrappers/Dashboard";
 import { BigSidebar, Navbar, SmallSidebar } from "../components";
+
 const DashboardLayout = () => {
+  const user = { name: "alex" };
+  const [showSidebar, setShowSidebar] = useState(false);
+  const [isDarkTheme, setIsDarkTheme] = useState(false);
+
+  const toggleDarkTheme = () => {
+    console.log("toggle dark theme");
+  };
+
+  const toggleSidebar = () => {
+    setShowSidebar(!showSidebar);
+  };
+
+  const logoutUser = async () => {
+    console.log("logout user");
+  };
+
   return (
     <Wrapper>
       <main className="dashboard">
