2d180acefa1f95326d404a8ff99fa01a8731b337
alexokin <alex.okin@gmail.com>
2023-09-05 11:51:40 +0300

    feature: job route and controller

--- server.js
+++ server.js
@@ -1,10 +1,50 @@
-import express from 'express'
-const app = express()
+import * as dotenv from "dotenv";
+dotenv.config();
+import express from "express";
+const app = express();
 
-app.get('/', (req,res)=>{
-    res.send('hello world')
-})
+import morgan from "morgan";
+import mongoose from "mongoose";
 
-app.listen(5100, () => {
-    console.log('server running...');
-})
+// Routers
+import jobRouter from './routes/jobRouter.js'
+
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("dev"));
+}
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("hello world");
+});
+    
+app.post("/", (req, res) => {
+  console.log(req);
+  res.json({ message: "data received", data: req.body });
+});
+
+app.use('/api/v1/jobs', jobRouter)
+
+app.use("*", (req, res) => {
+  res.status(404).json({ msg: "not found" });
+});
+
+app.use((err, req, res, next) => {
+  console.log(err);
+  res.status(500).json({ msg: "something went wrong" });
+});
+
+const port = process.env.PORT || 5100;
+
+try {
+    await mongoose.connect(process.env.MONGO_URL)
+    app.listen(port, () => {
+        console.log(`server running on PORT ${port}...`);
+      });
+} catch (error) {
+    console.log(error)
+    process.exit(1);
+}
+
+
