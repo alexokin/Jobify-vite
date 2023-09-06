46f9a4091a252377dcb5061058ab8541c6436e65
alexokin <alex.okin@gmail.com>
2023-09-06 19:33:31 +0300

    login logic

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,19 +1,27 @@
 import { StatusCodes } from "http-status-codes";
 import User from "../models/UserModel.js";
 import bcrypt from "bcryptjs";
-import { hashedPassword } from "../utils/passwordUtils.js";
+import { comparePassword, hashPassword } from "../utils/passwordUtils.js";
+import { UnauthenticatedError } from "../errors/customErrors.js";
 
 export const register = async (req, res) => {
   const isFirstAccount = (await User.countDocuments()) === 0;
   req.body.role = isFirstAccount ? "admin" : "user";
 
-  const hashedPassword = await hashedPassword(req.body.password)
-  req.body.password = hashedPassword
+  const hashedPassword = await hashPassword(req.body.password);
+  req.body.password = hashedPassword;
 
   const user = await User.create(req.body);
   res.status(StatusCodes.CREATED).json({ msg: "user created" });
 };
 
 export const login = async (req, res) => {
+  const user = await User.findOne({ email: req.body.email });
+  if (!user) throw new UnauthenticatedError("invalid credentials");
+  const isPasswordCorrect = await comparePassword(
+    req.body.password,
+    user.password
+  );
+  if (!isPasswordCorrect) throw new UnauthenticatedError("invalid credentials");
   res.send("login");
 };
--- middleware/validationMiddleware.js
+++ middleware/validationMiddleware.js
@@ -61,7 +61,18 @@ export const validateRegisterInput = withValidationErrors([
     .notEmpty()
     .withMessage("password is required")
     .isLength({ min: 8 })
-    .withMessage('password must be at least 8 characters long'),
+    .withMessage("password must be at least 8 characters long"),
   body("location").notEmpty().withMessage("location is required"),
   body("lastName").notEmpty().withMessage("last name is required"),
+]);
+
+export const validateLoginInput = withValidationErrors([
+  body("email")
+    .notEmpty()
+    .withMessage("email is required")
+    .isEmail()
+    .withMessage("invalid email format"),
+  body("password")
+    .notEmpty()
+    .withMessage("password is required")
 ]);
