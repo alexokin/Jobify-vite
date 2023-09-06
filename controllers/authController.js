e936b037dbe25274043441a2bdcf7316fcaa288b
alexokin <alex.okin@gmail.com>
2023-09-06 17:32:59 +0300

    setting up hashedPassword utils function

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,13 +1,19 @@
-import { StatusCodes } from "http-status-codes"
-import User from "../models/UserModel.js"
+import { StatusCodes } from "http-status-codes";
+import User from "../models/UserModel.js";
+import bcrypt from "bcryptjs";
+import { hashedPassword } from "../utils/passwordUtils.js";
 
+export const register = async (req, res) => {
+  const isFirstAccount = (await User.countDocuments()) === 0;
+  req.body.role = isFirstAccount ? "admin" : "user";
 
+  const hashedPassword = await hashedPassword(req.body.password)
+  req.body.password = hashedPassword
 
-export const register = async (req,res) => {
-    const user = await User.create(req.body)
-    res.status(StatusCodes.CREATED).json({user})
-}
+  const user = await User.create(req.body);
+  res.status(StatusCodes.CREATED).json({ msg: "user created" });
+};
 
-export const login = async (req,res) => {
-    res.send('login')
-}
+export const login = async (req, res) => {
+  res.send("login");
+};
