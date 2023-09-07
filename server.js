e224a0fa79bb373e5891123a21b81b22ce13bc7d
alexokin <alex.okin@gmail.com>
2023-09-07 16:38:31 +0300

    verify token and cookie

--- server.js
+++ server.js
@@ -6,26 +6,27 @@ const app = express();
 
 import morgan from "morgan";
 import mongoose from "mongoose";
-
+import cookieParser from "cookie-parser";
 
 // Routers
 import jobRouter from "./routes/jobRouter.js";
-import authRouter from "./routes/authRouter.js"
+import authRouter from "./routes/authRouter.js";
 //Middleware
 import errorHandlerMiddleware from "./middleware/errorHandlerMiddleware.js";
+import { authenticateUser } from "./middleware/authMiddleware.js";
 
 if (process.env.NODE_ENV === "development") {
   app.use(morgan("dev"));
 }
-
+app.use(cookieParser());
 app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("hello world");
 });
 
-app.use("/api/v1/jobs", jobRouter);
-app.use("/api/v1/auth", authRouter)
+app.use("/api/v1/jobs", authenticateUser, jobRouter);
+app.use("/api/v1/auth", authRouter);
 
 app.use("*", (req, res) => {
   res.status(404).json({ msg: "not found" });
