bcf680450e233c432dce394fe435587e66e3e6f6
alexokin <alex.okin@gmail.com>
2023-09-07 15:39:06 +0300

    http only cookie

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,6 +1,5 @@
 import { StatusCodes } from "http-status-codes";
 import User from "../models/UserModel.js";
-import bcrypt from "bcryptjs";
 import { comparePassword, hashPassword } from "../utils/passwordUtils.js";
 import { UnauthenticatedError } from "../errors/customErrors.js";
 import { createJWT } from "../utils/tokenUtils.js";
@@ -25,6 +24,12 @@ export const login = async (req, res) => {
   );
   if (!isPasswordCorrect) throw new UnauthenticatedError("invalid credentials");
   const token = createJWT({ userId: user._id, role: user.role });
+  const oneDay = 1000 * 60 * 60 * 24;
 
-  res.send("login");
+  res.cookie("token", token, {
+    httpOnly: true,
+    expires: new Date(Date.now() + oneDay),
+    secure: process.env.NODE_ENV === "production",
+  });
+  res.status(StatusCodes.OK).json({ msg: "user logged in" });
 };
