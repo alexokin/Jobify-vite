ce74dbd6916259355c623ab0925a4e9b77585c8d
alexokin <alex.okin@gmail.com>
2023-09-10 14:46:12 +0300

    proxy setup, vite config and custom fetch with axios

--- controllers/userController.js
+++ controllers/userController.js
@@ -4,16 +4,18 @@ import Job from "../models/JobModel.js";
 
 export const getCurrentUser = async (req, res) => {
   const user = await User.findOne({ _id: req.user.userId });
-  const userWithoutPassword = user.toJSON()
+  const userWithoutPassword = user.toJSON();
   res.status(StatusCodes.OK).json({ userWithoutPassword });
 };
 
 export const getApplicationStats = async (req, res) => {
-  res.status(StatusCodes.OK).json({ msg: "get application stats" });
+  const users = await User.countDocuments();
+  const jobs = await Job.countDocuments();
+  res.status(StatusCodes.OK).json({ users, jobs });
 };
 export const updateUser = async (req, res) => {
-    const obj = {...req.body}
-    delete obj.password
-    const updatedUser = await User.findByIdAndUpdate(req.user.userId, obj)
+  const obj = { ...req.body };
+  delete obj.password;
+  const updatedUser = await User.findByIdAndUpdate(req.user.userId, obj);
   res.status(StatusCodes.OK).json({ msg: "update user" });
 };
