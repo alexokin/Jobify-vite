723b2641dc1549dcd1986fbb0ef3385fac05edd1
alexokin <alex.okin@gmail.com>
2023-09-18 19:42:41 +0300

    edit job loader

--- client/src/pages/EditJob.jsx
+++ client/src/pages/EditJob.jsx
@@ -1,8 +1,29 @@
+import { FormRow, FormRowSelect } from "../components";
+import Wrapper from "../assets/wrappers/DashboardFormPage";
+import { useLoaderData } from "react-router-dom";
+import { JOB_STATUS, JOB_TYPE } from "../../../utils/constants";
+import { Form, useNavigation, redirect } from "react-router-dom";
+import { toast } from "react-toastify";
+import customFetch from "../utils/customFetch";
+
+export const loader = async ({ params }) => {
+  try {
+    const { data } = await customFetch.get(`/jobs/${params.id}`);
+    return data;
+  } catch (error) {
+    toast.error(error?.response?.data?.msg);
+    return redirect("/dashboard/all-jobs");
+  }
+};
+
+export const action = async () => {
+  return null;
+};
 
 const EditJob = () => {
-  return (
-    <div>EditJob</div>
-  )
-}
+  const { job } = useLoaderData();
+  console.log(job);
+  return <h1>EditJob</h1>;
+};
 
-export default EditJob
+export default EditJob;
