69c9c2c0560e77f4f5ecf74959154fc4e48f3088
alexokin <alex.okin@gmail.com>
2023-09-20 18:59:51 +0300

    pagination of the backend

--- controllers/jobController.js
+++ controllers/jobController.js
@@ -7,7 +7,6 @@ export const getAllJobs = async (req, res) => {
   const { search, jobStatus, jobType, sort } = req.query;
   const queryObject = {
     createdBy: req.user.userId,
-    
   };
 
   if (search) {
@@ -17,25 +16,38 @@ export const getAllJobs = async (req, res) => {
     ];
   }
 
-  if (jobStatus && jobStatus !== 'all') {
-    queryObject.jobStatus = jobStatus
+  if (jobStatus && jobStatus !== "all") {
+    queryObject.jobStatus = jobStatus;
   }
 
-  if (jobType && jobType !== 'all') {
-    queryObject.jobType = jobType
+  if (jobType && jobType !== "all") {
+    queryObject.jobType = jobType;
   }
 
   const sortOptions = {
-    newest: '-createdAt',
-    oldest: 'createdAt',
-    'a-z':'position',
-    'z-a':'-position'
-  }
+    newest: "-createdAt",
+    oldest: "createdAt",
+    "a-z": "position",
+    "z-a": "-position",
+  };
 
-  const sortKey = sortOptions[sort] || sortOptions.newest
+  const sortKey = sortOptions[sort] || sortOptions.newest;
 
-  const jobs = await Job.find(queryObject).sort(sortKey);
-  res.status(StatusCodes.OK).json({ jobs });
+  //pagination
+  const page = Number(req.query.page) || 1;
+  const limit = Number(req.query.limit) || 10;
+  const skip = (page - 1) * limit;
+
+  const jobs = await Job.find(queryObject)
+    .sort(sortKey)
+    .skip(skip)
+    .limit(limit);
+
+  const totalJobs = await Job.countDocuments(queryObject);
+  const numOfPages = Math.ceil(totalJobs / limit);
+  res
+    .status(StatusCodes.OK)
+    .json({ totalJobs, numOfPages, currentPage: page, jobs });
 };
 
 export const createJob = async (req, res) => {
