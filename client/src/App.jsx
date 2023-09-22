36382bbba4b0c415474d647bf10de6f64c2c23a0
alexokin <alex.okin@gmail.com>
2023-09-22 17:34:36 +0300

    first query with react query

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,6 @@
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import {
   HomeLayout,
   Landing,
@@ -25,12 +27,21 @@ import { loader as allJobsLoader } from "./pages/AllJobs";
 import { loader as editJobLoader } from "./pages/EditJob";
 import { loader as adminLoader } from "./pages/Admin";
 import { loader as statsLoader } from "./pages/Stats";
+import ErrorElement from "./components/ErrorElement";
 
 export const checkDefaultTheme = () => {
   const isDarkTheme = localStorage.getItem("darkTheme") === "true";
   document.body.classList.toggle("dark-theme", isDarkTheme);
 };
 checkDefaultTheme();
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 1000 * 60 * 5,
+    },
+  },
+});
 
 const router = createBrowserRouter([
   {
@@ -62,7 +73,12 @@ const router = createBrowserRouter([
             element: <AddJob />,
             action: addJobAction,
           },
-          { path: "stats", element: <Stats />, loader: statsLoader },
+          {
+            path: "stats",
+            element: <Stats />,
+            loader: statsLoader,
+            errorElement: <ErrorElement />,
+          },
           { path: "all-jobs", element: <AllJobs />, loader: allJobsLoader },
           { path: "profile", element: <Profile />, action: profileAction },
           { path: "admin", element: <Admin />, loader: adminLoader },
@@ -83,7 +99,12 @@ const router = createBrowserRouter([
 ]);
 
 const App = () => {
-  return <RouterProvider router={router} />;
+  return (
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router} />
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
+  );
 };
 
 export default App;
--- client/src/pages/Stats.jsx
+++ client/src/pages/Stats.jsx
@@ -1,18 +1,28 @@
+import { useQuery } from "@tanstack/react-query";
 import { ChartsContainer, StatsContainer } from "../components";
 import customFetch from "../utils/customFetch";
 import { useLoaderData } from "react-router-dom";
 
 export const loader = async () => {
-  try {
-    const response = await customFetch.get("/jobs/stats");
-    return response.data;
-  } catch (error) {
-    return error;
-  }
+  return null;
+  const response = await customFetch.get("/jobs/stats");
+  return response.data;
 };
 
 const Stats = () => {
-  const { defaultStats, monthlyApplications } = useLoaderData();
+  // const { defaultStats, monthlyApplications } = useLoaderData();
+
+  const { isLoading, isError, data } = useQuery({
+    queryKey: ["stats"],
+    queryFn: () => customFetch.get("/jobs/stats"),
+  });
+  if (isLoading) {
+    return <h4>Loading...</h4>;
+  }
+  if (isError) {
+    return <h4>Error...</h4>;
+  }
+  const { defaultStats, monthlyApplications } = data.data;
 
   return (
     <>
